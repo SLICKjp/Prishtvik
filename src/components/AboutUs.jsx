cd9605011ef6f4fefb5617429a99bca1fb4d625c
Jathin <jathinp74@gmail.com>
2024-10-16 19:12:29 +0530

    Updated About Us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,19 +8,19 @@ const AboutUs = forwardRef((props, ref) => (
     <div className="aboutus-content-container">
       <img className="aboutus-img" src={Aboutimg} alt="" />
 
-      <div className="text-[#ffffff] my-9">
+      <div className="text-[#ffffff] py-14">
         <div className="aboutUsSectionHeading mb-5 flex flex-row justify-start items-center">
-          <div className="aboutUsSectionBullet mr-5"></div> <p>About Us</p>{" "}
+          <div className="aboutUsSectionBullet mr-5"></div> <p className="text-4xl font-semibold">About Us</p>{" "}
           <div className="aboutUsSectionBullet ml-5"></div>
         </div>
-        <h3 className="aboutus-heading">We are Leading IT Service Solution.</h3>
+       
 
         <p className="aboutus-text">
-        Founded in 2015 as A1 Services, we became an authorized service partner for F1 Info Solutions Private Limited, dedicated to delivering top-notch IT support and solutions for leading OEM products, including Asus, BenQ, ViewSonic, HP, Dell, Lenovo, AOC, and more. Over the years, we have proudly served over 5,000 satisfied customers across Central Gujarat, providing reliable onsite support within a 150 km radius. With more than 30,000 service calls successfully attended too, we have built a strong reputation for trust and quality across various brands.
+         Founded in 2015 as A1 Services, we established ourselves as an authorized service partner for F1 Info Solutions Private Limited, dedicated to providing exceptional IT support and solutions for leading OEM products, including Asus, BenQ, ViewSonic, HP, Dell, Lenovo, AOC, and more.
 
-In 2024, we are excited to reintroduce ourselves as Prishtvik Info Solutions Pvt Ltd, expanding our offerings as a sales partner for corporate houses. We provide comprehensive solutions for active and passive networks, servers, online/offline UPS systems, surveillance, and IT infrastructure products.
+In 2024, we are proud to reintroduce ourselves as Prishtvik Info Solutions Pvt Ltd. With this new identity, we are expanding our offerings to serve as a trusted sales partner for corporate clients. Our expertise spans comprehensive solutions for active and passive networks, servers, online and offline UPS systems, surveillance, and IT infrastructure products.
 
-Our commitment to excellence drives us to continually innovate and enhance our services, ensuring that our customers receive the best possible support and tailored solutions to meet their needs. Join us in our mission to empower your technology experience!
+At Prishtvik Info Solutions, we are committed to empowering your technology experience. Join us on this journey to enhance your IT capabilities and drive your business forward!
         </p>
       </div>
     </div>
